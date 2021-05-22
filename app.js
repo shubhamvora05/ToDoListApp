c77dad0a844d836895546aca5ccfc4985a3a5b09
shubham vora <shubhamvora05@gmail.com>
2021-05-22 17:42:40 +0530

    todo list app

--- app.js
+++ app.js
@@ -9,16 +9,17 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://shubhamvora:Vora.@123@cluster0.x5gph.mongodb.net/ToDoList?retryWrites=true&w=majority",{useNewUrlParser: true},{ useUnifiedTopology: true })
+//mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
 };
 
-const Item = mongoose.model("Item", itemsSchema);
+const Item = mongoose.model("Item", itemsSchema); //Item inside bracket is the table name and Item variable is model name.
 
 
 const item1 = new Item({
@@ -135,12 +136,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
-
-app.listen(port, function() {
-  console.log("Server started Successfully");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
